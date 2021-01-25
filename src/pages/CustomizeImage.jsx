a1846798c426d923c85b8ca1bace34d98c2eb10f
tranttxuan <>
2021-01-25 18:05:20 +0100

    use Hook

--- src/pages/CustomizeImage.jsx
+++ src/pages/CustomizeImage.jsx
@@ -1,49 +1,100 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+// import "./CustomizeImage.css"
+
+// export default class CustomizeImage extends Component {
+//       state = {
+//             size: 0,
+//             url: ''
+//       }
+//       handleChange = (event) => {
+//             const { name, value } = event.target;
+//             this.setState({ [name]: value });
+//       }
+//       render() {
+//             const { size, url } = this.state;
+//             return (
+//                   <div className="customize-image-page">
+//                         <h1>Customize Image</h1>
+
+//                         <div>
+//                               <input
+//                                     name="url"
+//                                     value={url}
+//                                     onChange={this.handleChange}
+//                                     type="text"
+//                                     placeholder="Image Url"
+//                               />
+//                         </div>
+
+//                         <div>
+//                               <input
+//                                     name="size"
+//                                     value={size}
+//                                     onChange={this.handleChange}
+//                                     type="range"
+//                                     min={0}
+//                                     max={200}
+//                                     className="range"
+//                               />
+//                         </div>
+
+//                         {url && <p>{size} x {size} px</p>}
+
+//                         {url && <img src={url} width={size} height={size} alt='image' />}
 
 
-export default class CustomizeImage extends Component {
-      state = {
-            size: 0,
-            url: ''
-      }
-      handleChange = (event) => {
+
+//                   </div>
+//             )
+//       }
+// }
+
+
+
+import React from 'react'
+import { useState } from 'react'
+import "./CustomizeImage.css"
+function CustomizeImage() {
+      const [size, setSize] = useState('');
+      const [url, setUrl] = useState("");
+      const handleChange = (event) => {
             const { name, value } = event.target;
-            this.setState({ [name]: value });
+            name === "size" ? setSize(value) : setUrl(value)
       }
-      render() {
-            const { size, url } = this.state;
-            return (
-                  <div >
-                        <h1>Customize Image</h1>
+      return (
+            <div className="customize-image-page">
+                  <h1>Customize Image</h1>
 
-                        <div>
-                              <input
-                                    name="url"
-                                    value={url}
-                                    onChange={this.handleChange}
-                                    type="text"
-                              />
-                        </div>
-
-                        <div>
-                              <input
-                                    name="size"
-                                    value={size}
-                                    onChange={this.handleChange}
-                                    type="range"
-                                    min={0}
-                                    max={200}
-                                    className="range"
-                              />
-                        </div>
-
-                        <p>{size} x {size} px</p>
-
-                        <img src={url} width={size} height={size} alt='image' />
-
-
-
+                  <div>
+                        <input
+                              name="url"
+                              value={url}
+                              onChange={handleChange}
+                              type="text"
+                              placeholder="Image Url"
+                        />
                   </div>
-            )
-      }
+
+                  <div>
+                        <input
+                              name="size"
+                              value={size}
+                              onChange={handleChange}
+                              type="range"
+                              min={0}
+                              max={200}
+                              className="range"
+                        />
+                  </div>
+
+                  {url && <p>{size} x {size} px</p>}
+
+                  {url && <img src={url} width={size} height={size} alt='image' />}
+
+
+
+            </div>
+      )
 }
+
+export default CustomizeImage
--- src/pages/Temperature.jsx
+++ src/pages/Temperature.jsx
@@ -1,41 +1,89 @@
-import React, { Component } from 'react'
+// import React, { Component } from 'react';
+// import "./Temperature.css";
 
-export default class Temperature extends Component {
-      state = {
-            temperature: 0,
-            message: '',
-            color: ''
+// export default class Temperature extends Component {
+//       state = {
+//             temperature: 0,
+//             message: '',
+//             color: ''
 
-      }
-      handleChange = (event) => {
+//       }
+//       handleChange = (event) => {
+//             const temperature = event.target.value;
+//             this.setState({ temperature });
+//             if (temperature <= 10) {
+//                   this.setState({ message: "It's cold", color: "blue" });
+//                   return;
+//             } else if (temperature <= 30) {
+//                   this.setState({ message: "It's nice", color: "black" });
+//                   return;
+//             }else {
+//                   this.setState({ message: "It's warm", color: "red" });
+//                   return;
+//             }
+
+//       }
+//       render() {
+//             const { temperature, message, color } = this.state;
+//             return (
+//                   <div className="temp-page">
+//                         <h1>Temperature</h1>
+//                         <input
+//                               name="temperature"
+//                               value={temperature}
+//                               onChange={this.handleChange}
+//                               type="number"
+//                               placeholder="Temperature in C"
+//                         />
+//                         <p style={{ color }}>{message}</p>
+//                   </div>
+//             )
+//       }
+// }
+
+
+
+import React from 'react'
+import { useState } from 'react'
+import "./Temperature.css";
+
+function Temperature() {
+      const [temperature, setTemperature] = useState(0);
+      const [message, setMessage] = useState("");
+      const [color, setColor] = useState('')
+
+      const handleChange = (event) => {
             const temperature = event.target.value;
-            this.setState({ temperature });
+            setTemperature(temperature);
             if (temperature <= 10) {
-                  this.setState({ message: "It's cold", color: "blue" });
+                  setMessage("It's cold");
+                  setColor("blue")
+
                   return;
             } else if (temperature <= 30) {
-                  this.setState({ message: "It's nice", color: "black" });
+                  setMessage("It's nice");
+                  setColor("black")
                   return;
-            }else {
-                  this.setState({ message: "It's warm", color: "red" });
+            } else {
+                  setMessage("It's warm");
+                  setColor("red")
                   return;
             }
-
+      
       }
-      render() {
-            const { temperature, message, color } = this.state;
-            return (
-                  <div>
-                        <h1>Temperature</h1>
-                        <input
-                              name="temperature"
-                              value={temperature}
-                              onChange={this.handleChange}
-                              type="number"
-                              placeholder="Temperature in C"
-                        />
-                        <p style={{ color }}>{message}</p>
-                  </div>
-            )
-      }
+      return (
+            <div className="temp-page">
+                  <h1>Temperature</h1>
+                  <input
+                        name="temperature"
+                        value={temperature}
+                        onChange={handleChange}
+                        type="number"
+                        placeholder="Temperature in C"
+                  />
+                  <p style={{ color }}>{message}</p>
+            </div>
+      )
 }
+
+export default Temperature
